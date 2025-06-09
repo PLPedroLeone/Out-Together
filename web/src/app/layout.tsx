524795601e9bdbe7140a2181231947dd38e8f783
Buffalopeter <pedroleone95@gmail.com>
2025-06-09 16:04:54 -0300

    clarity added

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import NavBar from "@/components/NavBar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,6 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rwx8dif4c8");
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
           <NavBar />
           {children}
